bc6835808b4425f69e76ea2c5a4d095cc4645b37
cedim1 <164538920+cedim1@users.noreply.github.com>
2024-03-23 09:33:39 +0000

    Create CreateAlayout.js

---
+++ CreateAlayout.js
@@ -0,0 +1,18 @@
+const text = document.createElement("textarea");
+text.id = "te";
+document.body.appendChild(text);
+const button = document.createElement("button");
+button.id = "Go";
+button.innerText = "Send"
+button.onclick="sort()"
+document.body.appendChild(button);
+function sort() {
+  if (database != document.getElementById("te"))
+  const doh = document.createElement("p");
+doh.innerText = "Hmm, I don't know that one, adding to database";
+document.body.appendChild(doh);
+} else {
+ const doh = document.createElement("p");
+doh.innerText = database;
+document.body.appendChild(doh);
+}
